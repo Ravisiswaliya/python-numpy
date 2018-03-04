f3317926fdcccb2a05991e6abae158eced1d04a6
ravisiswaliya <ravisiswaliya@gmail.com>
2018-03-04 11:07:41 +0530

    selecting element

---
+++ array_elements.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+#creating a matrix
+matrix = np.array([[10,20,30],
+                   [40,50,60],
+                   [71,82,93]])
+#print(matrix[1,2])
+
+#creating a tensor
+
+tensor = np.array([
+    [[[13,23],[11,31]],[[23,14],[34,43]]],
+    [[[22,23],[36,35]],[[44,34],[52,55]]]
+])
+
+#print(tensor)
+#select element from a tensor
+print(tensor[1,0,1])
